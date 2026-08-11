960ff8adbe87a49b6c9bb68d1a4597a555a9d0a9
b1239499 <49159352+b1239499@users.noreply.github.com>
2026-08-12 01:18:38 +0800

    Delete src/main directory

--- src/main/java/com/fkc/game/uno/model/UnoSettings.java
+++
@@ -1,9 +0,0 @@
-package com.fkc.game.uno.model;
-
-public class UnoSettings {
-    public int startingHandSize = 7;
-    public int targetScore = 500;
-    public int thinkingTimeSeconds = 30;
-    /** How many cards you draw as a penalty if someone catches you not having called "UNO" at 1 card. */
-    public int unoCatchPenalty = 2;
-}
